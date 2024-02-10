d0cf4d6e5333b065bea6fca16570808ee17b67b8
ustato <atsuto.inage@acompany-ac.com>
2024-02-10 20:47:33 +0900

    add kubeconfig

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,7 @@ output "kube_config" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
   sensitive = true
 }
+
+output "attestation_uri" {
+  value = azurerm_attestation_provider.attestation.attestation_uri
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,21 +8,21 @@ variable "location" {
   type        = string
   nullable    = false
   description = "The region where the infrastructure will be built."
-  default     = "eastus"
+  default     = "japanwest"
 }
 
 variable "vm_size" {
   type        = string
   nullable    = false
   description = "VM type to choose from https://learn.microsoft.com/ja-jp/azure/virtual-machines/dcv3-series#dcdsv3-series."
-  default     = "Standard_DC2ds_v3"
+  default     = "Standard_DC4ds_v3"
 }
 
 variable "node_count" {
   type        = number
   nullable    = false
   description = "The initial quantity of nodes for the node pool."
-  default     = 2
+  default     = 1
 }
 
 variable "username" {
